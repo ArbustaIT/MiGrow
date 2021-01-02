2a8fa8c602ad19f922d0ec424578bd19543a0813
Nahuel Perez <36615599+Nahuelpack@users.noreply.github.com>
2021-01-02 15:23:32 -0300

    Cache data

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ $(() => {
     if (nowst) {
         nowst = atob(nowst.replace('?', ''))
 
-        if (nowst.includes('arbusta.net')||nowst.includes('test')) {
+        if (nowst.includes('arbusta.net') || nowst.includes('test')) {
             //Pedir los datos y abrir la pagina
+
             getInfo(nowst).then((dd) => {
-                console.log(dd)
                 window.sessionStorage.setItem('info', JSON.stringify(dd))
-                window.sessionStorage.setItem('user', dd.name)
+                window.sessionStorage.setItem('user', nowst)
                 getPage('page/grow.htm')
                     .then((r) => {
                         $('body').html('<div id="grow" class="page">' + r + '</div>');
@@ -65,16 +65,26 @@ getPage = (x) => {
 
 //Obteniendo json del user
 getInfo = (x) => {
-    return new Promise((resolve, regect) => {
-        fetch('https://script.google.com/a/macros/arbusta.net/s/AKfycbwVeOng24GwZZUuC_Ef6tPJ0HpHlBimimkOhisAxQXMpmVYDsE0Wu3kIg/exec?' + x, { cache: 'no-cache' }).then(Response => {
-            Response.json().then(json => {
-                resolve(json)
+
+    return xa = new Promise((resolve, regect) => {
+
+        if (window.sessionStorage.getItem('user') == x) {
+            console.log('Local')
+            resolve(JSON.parse(window.sessionStorage.getItem('info')))
+        } else {
+            console.log('Batabase')
+            fetch('https://script.google.com/a/macros/arbusta.net/s/AKfycbwVeOng24GwZZUuC_Ef6tPJ0HpHlBimimkOhisAxQXMpmVYDsE0Wu3kIg/exec?' + x, { cache: 'no-cache' }).then(Response => {
+                Response.json().then(json => {
+                    resolve(json)
+                })
+            }).catch(err => {
+                regect(err)
             })
-        }).catch(err => {
-            regect(err)
-        })
+        }
+
     })
 }
+
 
 
 
